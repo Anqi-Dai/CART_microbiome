e374110f67d20d4ad48fea3aed2e9e91d643d33a
Anqi-Dai <adai@bu.edu>
2019-10-17 22:13:26 -0400

    update

--- scripts/command.sh
+++
@@ -1,9 +0,0 @@
-humann2_barplot  \
-    --input $1 \
-    --focal-feature $2 \
-    --sort similarity metadata \
-    --focal-metadatum Toxicity \
-    --last-metadatum Toxicity \
-    --scaling pseudolog \
-    --dimensions 10 5 \
-    -o $2.png
