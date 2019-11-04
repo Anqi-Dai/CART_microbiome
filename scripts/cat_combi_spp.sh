2952e46a9a786dc354604a2436e21823e34788c3
Anqi-Dai <adai@bu.edu>
2019-11-04 15:35:34 -0500

    cladogram

--- scripts/cat_combi_spp.sh
+++ scripts/cat_combi_spp.sh
@@ -1,2 +1,2 @@
-cat ../data/lefse_input/pheno__CR__Disease_Class.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered.tsv > ../data/lefse_input/spp_abundace_pheno__CR__Disease_Class.tsv
-cat ../data/lefse_input/pheno__Toxicity__Disease_Class.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered.tsv > ../data/lefse_input/spp_abundace_pheno__Toxicity__Disease_Class.tsv
+cat ../data/lefse_input/pheno__CR.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered_w_taxa.tsv > ../data/lefse_input/spp_abundace_pheno__CR.tsv
+cat ../data/lefse_input/pheno__Toxicity.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered_w_taxa.tsv > ../data/lefse_input/spp_abundace_pheno__Toxicity.tsv
---
+++ scripts/plotting_cladogram_spp.sh
@@ -0,0 +1,2 @@
+plot_cladogram.py ../data/lefse_input/spp_abundace_pheno__CR.res  ../figs/cladogram/spp_abundace_lefse_pheno__CR.png  --dpi 300 --format png --title pheno__CR. --label_font_size 3
+plot_cladogram.py ../data/lefse_input/spp_abundace_pheno__Toxicity.res  ../figs/cladogram/spp_abundace_lefse_pheno__Toxicity.png  --dpi 300 --format png --title pheno__Toxicity. --label_font_size 3
