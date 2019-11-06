035105424056b567d4399136c6cfc74c2fbe183c
Anqi-Dai <adai@bu.edu>
2019-11-06 18:17:36 -0500

    update

--- scripts/cat_combi_spp.sh
+++ scripts/cat_combi_spp.sh
@@ -1,2 +1,2 @@
-cat ../data/lefse_input/pheno__CR.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered_w_taxa.tsv > ../data/lefse_input/spp_abundace_pheno__CR.tsv
-cat ../data/lefse_input/pheno__Toxicity.txt ../data/metaphlan2_result/merged_abundance_table_species_reordered_w_taxa.tsv > ../data/lefse_input/spp_abundace_pheno__Toxicity.tsv
+cat ../data/lefse_input/pheno__CR.txt ../data/metaphlan2_result/merged_abundance_table_WHOLE_reordered.tsv > ../data/lefse_input/spp_abundace_pheno__CR.tsv
+cat ../data/lefse_input/pheno__Toxicity.txt ../data/metaphlan2_result/merged_abundance_table_WHOLE_reordered.tsv > ../data/lefse_input/spp_abundace_pheno__Toxicity.tsv
