d943e6155087979b69feacaf80b112f4209419a4
Anqi-Dai <adai@bu.edu>
2019-11-21 14:12:37 -0500

    no subclass for the pathway lefse

--- scripts/cat_combi_CT.sh
+++ scripts/cat_combi_CT.sh
@@ -1,2 +1,2 @@
-cat ../data/lefse_input/pheno__CR__Disease_Class.txt ../data/pheno_aligned_relab_table/humann2_pathabundance_cpm_unstratified.tsv > ../data/lefse_input/pathway_abundace_pheno__CR__Disease_Class.tsv
-cat ../data/lefse_input/pheno__Toxicity__Disease_Class.txt ../data/pheno_aligned_relab_table/humann2_pathabundance_cpm_unstratified.tsv > ../data/lefse_input/pathway_abundace_pheno__Toxicity__Disease_Class.tsv
+cat ../data/lefse_input/pheno__CR.txt ../data/pheno_aligned_relab_table/humann2_pathabundance_cpm_unstratified.tsv > ../data/lefse_input/pathway_abundace_pheno__CR.tsv
+cat ../data/lefse_input/pheno__Toxicity.txt ../data/pheno_aligned_relab_table/humann2_pathabundance_cpm_unstratified.tsv > ../data/lefse_input/pathway_abundace_pheno__Toxicity.tsv
--- scripts/plotting_de_feature_CT.sh
+++ scripts/plotting_de_feature_CT.sh
@@ -1,2 +1,2 @@
-plot_res.py ../data/lefse_input/pathway_abundace_pheno__CR__Disease_Class.res  ../figs/pathway_abundace_pheno__CR__Disease_Class.png --feature_font_size 4 --width 10 --dpi 300 --title pheno__CR__Disease_Class.
-plot_res.py ../data/lefse_input/pathway_abundace_pheno__Toxicity__Disease_Class.res  ../figs/pathway_abundace_pheno__Toxicity__Disease_Class.png --feature_font_size 4 --width 10 --dpi 300 --title pheno__Toxicity__Disease_Class.
+plot_res.py ../data/lefse_input/pathway_abundace_pheno__CR.res  ../figs/pathway_abundace_pheno__CR.png --feature_font_size 4 --width 10 --dpi 300 --title pheno__CR.
+plot_res.py ../data/lefse_input/pathway_abundace_pheno__Toxicity.res  ../figs/pathway_abundace_pheno__Toxicity.png --feature_font_size 4 --width 10 --dpi 300 --title pheno__Toxicity.
